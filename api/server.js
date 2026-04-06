34fe38d5c55b52a2ef0c805cbee8b2a977d94c4b
caneryaray <yaray_caner@hotmail.com>
2026-04-06 18:22:48 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,19 @@
 // server için gerekli olanları burada ayarlayın
 
 // posts router'ını buraya require edin ve bağlayın
+
+const express = require('express');
+const cors = require('cors');
+
+// posts router'ını buraya require edin ve bağlayın
+const postsRouter = require('./posts/posts-router');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+// router bağlama
+server.use('/api/posts', postsRouter);
+
+module.exports = server;
